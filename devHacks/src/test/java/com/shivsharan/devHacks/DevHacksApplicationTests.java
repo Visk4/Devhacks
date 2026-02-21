e57e0c61723aaa2ba842e5e530b86702a10e4627
ShivsharanSanjawad <158738950+ShivsharanSanjawad@users.noreply.github.com>
2026-02-21 13:27:44 +0530

    backend intial

---
+++ devHacks/src/test/java/com/shivsharan/devHacks/DevHacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivsharan.devHacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevHacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
